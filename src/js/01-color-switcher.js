d98cb06ac802b26d304cd5f8206612af43139ed5
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2023-10-26 22:04:24 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+  
+  const startBtn = document.querySelector('button[data-start]');
+  const finBtn = document.querySelector('button[data-stop]');
+  let timerId = null;
+  finBtn.disabled = true;
 
+  function changeColor(){
+    document.body.style.backgroundColor = getRandomHexColor();
+  };
+
+  startBtn.addEventListener('click', ()=>{
+    startBtn.disabled = true;
+    finBtn.disabled = false;
+    timerId = setInterval(changeColor, 500);
+  });
+
+  finBtn.addEventListener('click', ()=>{
+    startBtn.disabled = false;
+    clearInterval(timerId);
+    return finBtn.disabled = true;
+  })
+ 
